494a57462d94bfe1ae8d94b2d0b0dc114fd965f7
Ajaj Rajguru <ajaj@ajency.in>
2017-07-04 18:17:01 +0530

    image zoom indentifier added

--- js/custom.js
+++ js/custom.js
@@ -131,6 +131,10 @@ $(function(){
 			type: 'image',
 			gallery:{
 			enabled:true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300 // don't foget to change the duration also in CSS
 			}
 		});
 
