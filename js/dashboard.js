a7b53f60dc9477a9f66f1c67548ebbb238936c77
jeromie <jeromie@ajency.in>
2017-08-22 18:08:44 +0530

    row select and update status

--- js/dashboard.js
+++ js/dashboard.js
@@ -131,7 +131,11 @@ function init_DataTables() {
 
 	// Approval Table
 	var approval_table = $('#datatable-listing_approval').DataTable({
-		"order": [[ 3, 'desc' ]],
+		"order": [[ 4, 'desc' ]],
+		"select": {
+			"style": 'multi',
+			"selector": 'td:first-child'
+		},
 		// "dom": 'lrtip',
 		// "scrollX": true,
 		"columnDefs": [
@@ -139,6 +143,11 @@ function init_DataTables() {
 		  	"targets": 'no-sort',
 		  	"orderable": false
 		  },
+		  {
+          	"orderable": false,
+          	"className": 'select-checkbox',
+          	"targets": 0
+		  }
   		  // {
   		  // 	"targets": [1,2,3,4,5,6,7,8],
   		  // 	"searchable": false
@@ -148,6 +157,26 @@ function init_DataTables() {
 	approval_table.columns().iterator( 'column', function (ctx, idx) {
 		$( approval_table.column(idx).header() ).append('<span class="sort-icon"/>');
 	} );
+
+	approval_table.on("click", "th.select-checkbox", function() {
+	    if ($("th.select-checkbox").hasClass("selected")) {
+	        approval_table.rows().deselect();
+	        $("th.select-checkbox").removeClass("selected");
+	    } else {
+	        approval_table.rows().select();
+	        $("th.select-checkbox").addClass("selected");
+	    }
+	}).on("select deselect", function() {
+	    ("Some selection or deselection going on")
+	    if (approval_table.rows({
+	            selected: true
+	        }).count() !== approval_table.rows().count()) {
+	        $("th.select-checkbox").removeClass("selected");
+	    } else {
+	        $("th.select-checkbox").addClass("selected");
+	    }
+	});
+
 	$('#listingNameSearch').on( 'keyup', function () {
 	    approval_table
 	        .columns( 0 )
@@ -185,6 +214,13 @@ function init_Multiselect() {
 	      $('#datatable-categories, #datatable-locations, #datatable-listing_approval').DataTable().column(col).search(
 	        search, true, false
 	      ).draw();
+
+	      // Show/hide first column for Listing Approval table
+	      if (selected == "Pending Review"){
+	      	$(".select-checkbox").css("display", "table-cell");
+	      }else{
+	      	$(".select-checkbox").css("display", "none");
+	      }
 	    }
   	});
 };
